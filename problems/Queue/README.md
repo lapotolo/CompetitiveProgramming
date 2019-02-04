c7620b92b8d364c1e7a7775fed9c7a0c92a5a8ba
lapotolo <lapotoloni@gmail.com>
2019-02-04 23:37:53 +0100

    no more greedy left. They were fun

--- problems/Queue/README.md
+++ problems/Queue/README.md
@@ -2,8 +2,13 @@
 Problem : http://codeforces.com/problemset/problem/141/C?locale=en
 
 ### Description:
+After having parsed the input, the people are sorted by the number of people taller than them and in front of them in the previous queue.
+By sorting we get two things: we can check if a possible solution exists and if it so we order we have is a valid one for the new queue.
+Then we have to assign heights to every person
+Starting from the first position that is the front of the queue, each of them are inserted into the height vector at position i-a[i].  
+The procedure is correct since inserting at the back of an correct right part of a queue does not influence the already correctly build part.
 
 # Time cost:
-O()
+O(n * log n) dominated by the sorting
 # Space cost:
-O()
+O(2 * n)
---
+++ problems/Queue/queue_bit.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#define printele(x) std::cout << x << std::endl;
+#define printdbg(x) std::cout << #x ": " << x << std::endl;
+#define printarr(a, start) /*std::cout<< #a ": ";*/ for(int i = start; i < a.size(); ++i) {std::cout<<a[i]<<" ";}  std::cout<<" : " #a <<"\n";
+#define printarr_pair_first(a, start) /*std::cout<< #a ": ";*/ for(int i = start; i < a.size(); ++i) {std::cout<<a[i].first<<" ";}  std::cout<<" : " #a <<"\n";
+
+#define printarr_pair_sec(a, start) /*std::cout<< #a ": ";*/ for(int i = start; i < a.size(); ++i) {std::cout<<a[i].second<<" ";}  std::cout<<" : " #a <<"\n";
+
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+
+  int64_t n;
+  std::cin >> n;
+  std::vector<std::pair<std::string, int> > people(n);
+  std::vector<int> heights(0, 0);
+
+
+  for(size_t i = 0; i < n; ++i) { std::cin >> people[i].first >> people[i].second; };
+	
+	auto m_cmp = [](std::pair<std::string, int> const& a, std::pair<std::string, int> const& b ) -> bool 
+		{
+			return a.second < b.second;
+		};
+
+	std::sort(people.begin(), people.end(), m_cmp); // sort to check if a solution exists
+
+  for(size_t i = 0; i < n; i++) if(people[i].second > i) { std::cout << -1 << std::endl; return 0; }
+	
+	// I need to go back "people[i].second" positions 
+	for(size_t i = 0; i < n; ++i) heights.insert( heights.begin() + i - people[i].second, i);
+	for(size_t i = 0; i < n; ++i) people[heights[i]].second = i + 1;
+	for(size_t i = 0; i < n; ++i) std::cout << people[i].first << " " << people[i].second << std::endl;
+  
+	return 0;
+}
