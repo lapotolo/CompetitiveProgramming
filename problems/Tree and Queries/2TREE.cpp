a956aed2a41ab2e6d9ebd70be77aa8c7f3a4c344
lapotolo <lapotoloni@gmail.com>
2019-02-10 15:59:01 +0100

    375D readme up

--- problems/Tree and Queries/2TREE.cpp
+++ problems/Tree and Queries/2TREE.cpp
@@ -5,29 +5,25 @@
 
 #define printele(x) std::cout << x << std::endl;
 #define printdbg(x) std::cout << #x ": " << x << std::endl;
-#define printarr(a, start) /*std::cout<< #a ": ";*/ for(int i = start; i < a.size(); ++i) {std::cout<<a[i]<<" ";}  std::cout<<" : " #a <<"\n";
-#define printarr_pair_first(a, start) /*std::cout<< #a ": ";*/ for(int i = start; i < a.size(); ++i) {std::cout<<a[i].first<<" ";}  std::cout<<" : " #a <<"\n";
-#define printarr_pair_sec(a, start) /*std::cout<< #a ": ";*/ for(int i = start; i < a.size(); ++i) {std::cout<<a[i].second<<" ";}  std::cout<<" : " #a <<"\n";
-
+#define printarr(a, start) for(int i = start; i < a.size(); ++i) {std::cout<<a[i]<<" ";}  std::cout<<" : " #a <<"\n";
 
 struct query {
   int l, r, k;
   size_t id;
 };
 
+// Sorry, I dont like doing these global declarations too :(
 std::vector<int> color;                  // vertices colors
 std::vector<std::vector<int>> tree;      // adjacency list
 std::vector<query> Qs;
-
-
-std::vector<int> freq;        // frequency
-std::vector<int> freqK;       // frequency of k
+std::vector<int> freq;        // freq[i] counts how many times we saw color i
+std::vector<int> freq_counter;       // freq_counter[i] counts how many colors we saw i times
 std::vector<int> solution;    // query solutions
 std::vector<int> dfs_grey;    // discovery time of a node. Here starts its subtree
 std::vector<int> dfs_black;   // Closing time of a node. Here finishes its subtree
-std::vector<int> flat_tree;   // tree flattened to handle 1d query
+std::vector<int> flat_tree;   // tree flattened to handle 2d queries
 
-void dfs(int u, int parent, int & time) {
+void dfs(int u, int parent, int & time) { // dfs of the tree to flatten it in an array.
   dfs_grey[u] = ++time;
   flat_tree[time] = color[u];
   for(int & node : tree[u]) if(node != parent) dfs(node, u, time);
@@ -40,16 +36,12 @@ int main() {
   int N, M, bucket_size = 512, time = 0;
   std::cin >> N >> M; // |V|, |E|
 
-  // Allocate memory
   color.resize(N+1);
   tree.resize(N+1);
   Qs.resize(M);
   solution.resize(M+1, 0);
-  freq.resize(10);
-  freqK.resize(10);
-
-  // freq.resize(1e5+1, 0);
-  // freqK.resize(1e5+1, 0);
+  freq.resize(100001, 0);
+  freq_counter.resize(100001, 0);
   dfs_grey.resize(N+1, 0);
   dfs_black.resize(N+1, 0);
   flat_tree.resize(N+1, 0);
@@ -68,9 +60,9 @@ int main() {
 
   // Start dfs from root 0
   dfs(0, -1, time);
-  printarr(flat_tree,0);
-  printarr(dfs_grey,0);
-  printarr(dfs_black,0);
+  // printarr(flat_tree,0);
+  // printarr(dfs_grey,0);
+  // printarr(dfs_black,0);
 
   // Read and create queries
   for(size_t i = 0; i < M; ++i) {
@@ -83,7 +75,6 @@ int main() {
                  , i
                  };
   }
-  //for (auto & q : Qs) std::cout << "(" <<q.l <<", " << q.r << ", " << q.k << ", " << q.id << ")\n";  
 
   auto mo_cmp = [&](query const& a, query const& b) -> bool
     {
@@ -95,18 +86,15 @@ int main() {
   // Sort queries by bucket
   std::sort(Qs.begin(), Qs.end(), mo_cmp);
   
-  //for (auto & q : Qs) std::cout << "(" <<q.l <<", " << q.r << ", " << q.k << ", " << q.id << ")\n";  
-
-  // Current left and right
   auto add = [&](int pos)     // Increment window
   {
     freq[flat_tree[pos]]++; // freq[i] counts the occurrences of  color i in the current window
-    freqK[freq[flat_tree[pos]]]++; // freqK[i] counts h
+    freq_counter[freq[flat_tree[pos]]]++; // freq_counter[i] counts h
   };
 
   auto remove = [&](int pos)  // Decrement window
   {
-    freqK[freq[flat_tree[pos]]]--;
+    freq_counter[freq[flat_tree[pos]]]--;
     freq[flat_tree[pos]]--;
   };
 
@@ -114,38 +102,22 @@ int main() {
   int curr_r = curr_l-1;
 
   for(size_t i = 0; i < M; ++i) {
-    std::cout << "(" <<Qs[i].l <<", " << Qs[i].r << ", " << Qs[i].k << ", " << Qs[i].id << ")\n";
-    printdbg(curr_l); printdbg(curr_r);
-    printdbg(Qs[i].l); printdbg(Qs[i].r);
-
-    while(curr_l > Qs[i].l) { curr_l--; add(curr_l); }
-    printele("After add left")
-    printarr(freq,0);
-    printarr(freqK,0);
-
-    while(curr_r < Qs[i].r) { curr_r++; add(curr_r); }
-    printele("After add right")
-    printarr(freq,0);
-    printarr(freqK,0);
-
-    
-    while(curr_l < Qs[i].l) { remove(curr_l); curr_l++; }
-    printele("After remove left")
-    printarr(freq,0);
-    printarr(freqK,0);
-
-
-    while(curr_r > Qs[i].r) { remove(curr_r); curr_r--; }
-    printele("After remove left")
-    printarr(freq,0);
-    printarr(freqK,0);
-    printele("");
-    printele("*******************************************************************")
-
-
-
-
-    solution[Qs[i].id] = freqK[Qs[i].k];
+    // std::cout << "(" <<Qs[i].l <<", " << Qs[i].r << ", " << Qs[i].k << ", " << Qs[i].id << ")\n";
+    // printdbg(curr_l); printdbg(curr_r);
+    // printdbg(Qs[i].l); printdbg(Qs[i].r);
+    // printele("starting from freq-freq_counter");
+    // printarr(freq,0);
+    // printarr(freq_counter,0);
+    while(curr_l > Qs[i].l) { /* printele("ADD L"); */ curr_l--; add(curr_l); }
+    while(curr_r < Qs[i].r) { /* printele("ADD R"); */ curr_r++; add(curr_r); }
+    while(curr_l < Qs[i].l) { /* printele("REM L"); */ remove(curr_l); curr_l++; }  
+    while(curr_r > Qs[i].r) { /* printele("REM R"); */ remove(curr_r); curr_r--; }
+    //std::cout << "Every operation finished for query " << i << std::endl;
+    //printarr(freq,0);
+    //printarr(freq_counter,0);
+    //printele("");
+    //printele("*******************************************************************")
+    solution[Qs[i].id] = freq_counter[Qs[i].k];
   }
 
   for(size_t i = 0; i < M; ++i) 
--- problems/Tree and Queries/README.md
+++ problems/Tree and Queries/README.md
@@ -1,10 +1,22 @@
-  Solution description:
+# Tree and queries
+Problem: 
 
-    Mo's algorithm over a tree using color frequencies count.
+### Description:
+We start by doing a Preorder traversal of the given tree to flatten it.
 
-  Time  complexity: O((N+Q)*sqrt(Q))
-  Space complexity: O(N+Q)
+For every node the discovery time and the finishing time corresponds respectively to the start and to the end of its rooted subtree.
 
-  Where:
-  - N is the size of the tree
-  - Q is the number of queries
+The tree flattening is useful in order to be able to process subtree queries as range queries on an array.
+
+To get the required performaces constraints we relies on Mo's algorithm to sort the queries.
+
+The typical `ADD` and `REMOVE` operations used in this kind of problems to mantain the window of current-in-range items
+are here implemented as counter for two frequencies vector:
+
+- `freq` => `freq[i]` counts how many times we saw color `i` 
+- `freq_counter` => `freq_counter[i]` counts how many colors we saw i times
+
+# Time cost:
+O( (n + m) * sqrt(m))
+# Space cost:
+O(n + m)
